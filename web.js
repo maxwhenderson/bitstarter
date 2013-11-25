aee7559aabb2946bfdc50cebfae760c44a39387c
Maximus <mwh33@cornell.edu>
2013-11-25 06:52:45 +0000

    Aded file read buffer

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2! I\'m the greatest');
+   var fileBuff = new Buffer();
+   fileBuff.write(fs.readFileSync('/index.html'));
+
+   response.send(fileBuff.toString());
 });
 
 var port = process.env.PORT || 5000;
